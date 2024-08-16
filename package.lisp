6a9ed531266f38a53792b41b96681dc5939b343f
Bruno Dias <dias.h.bruno@gmail.com>
2024-08-16 10:01:32 -0300

    select by tag name.

--- package.lisp
+++ package.lisp
@@ -10,59 +10,65 @@
 
 (defun match-selector (selector target)
   (when target
-    (case (car selector)
-      (:and (reduce (lambda (acc selector)
-                      (and acc (match-selector selector target)))
-                    (cdr selector)
-                    :initial-value t))
-      (t (destructuring-bind (kind . value)
-             selector
-           (case kind
-             (:id
-              (string-equal value
-                            (html5-parser:element-attribute target "id")))
-             (:class
-              (let ((class-names (str:words (html5-parser:element-attribute target "class"))))
-                (member value class-names :test #'string-equal)))))))))
+    (if (equal :and (car selector))
+	(reduce (lambda (acc selector)
+		  (and acc (match-selector selector target)))
+		(cdr selector)
+		:initial-value t)
+	(destructuring-bind (kind . value)
+	    selector
+	  (case kind
+	    (:tag-name
+	     (string-equal value
+			   (html5-parser:node-name target)))
+	    (:id
+	     (string-equal value
+			   (html5-parser:element-attribute target "id")))
+	    (:class
+	     (let ((class-names (str:words (html5-parser:element-attribute target "class"))))
+	       (member value class-names :test #'string-equal))))))))
 
 (defun reduce-tree (fn tree &key initial-value)
   (etypecase tree
     (html5-parser::document
      (reduce (lambda (acc tree) (reduce-tree fn tree :initial-value acc))
-             (html5-parser::%node-child-nodes tree)
-             :initial-value nil))
+	     (html5-parser::%node-child-nodes tree)
+	     :initial-value nil))
     (html5-parser::element
      (reduce (lambda (acc tree) (reduce-tree fn tree :initial-value acc))
-             (html5-parser::%node-child-nodes tree)
-             :initial-value (funcall fn initial-value tree)))
+	     (html5-parser::%node-child-nodes tree)
+	     :initial-value (funcall fn initial-value tree)))
     (html5-parser::text-node (funcall fn initial-value tree))
     (t initial-value)))
 
 (defun make-selector (selector)
-  (cons (case (char selector 0) (#\. :class) (#\# :id))
-        (subseq selector 1)))
+  (let ((cat (case (char selector 0) (#\. :class) (#\# :id) (t :tag-name))))
+    (cons cat
+	  (if (equal cat :tag-name)
+	      selector
+	      (subseq selector 1)))))
 
 (defun specialized-selector (name)
-  (let ((selectors (cl-ppcre:all-matches-as-strings "[\\#\\.]([\\w\\-\\_0-9]+)" name)))
+  (let ((selectors (cl-ppcre:all-matches-as-strings "[\\#\\.]?([\\w\\-\\_0-9]+)" name)))
     (reduce (lambda (acc selector)
-              (append acc (list (make-selector selector))))
-            (cdr selectors)
-            :initial-value (list (make-selector (car selectors))))))
+	      (append acc (list (make-selector selector))))
+	    (cdr selectors)
+	    :initial-value (list (make-selector (car selectors))))))
 
 (defun parse-selector (name)
   (let ((groups (str:words name)))
     (cons :and (reduce (lambda (acc selector)
-                         (list* acc (specialized-selector selector)))
-                       (cdr groups)
-                       :initial-value (specialized-selector (car groups))))))
+			 (list* acc (specialized-selector selector)))
+		       (cdr groups)
+		       :initial-value (specialized-selector (car groups))))))
 
 (defun query-selector (selector tree)
   (let ((matcher (parse-selector selector)))
     (reduce-tree (lambda (acc element)
-                   (etypecase element
-                     (html5-parser::text-node acc)
-                     (t (progn
-                          (when (match-selector matcher element)
-                            (setf acc (append acc (list element))))
-                          acc))))
-                 tree)))
+		   (etypecase element
+		     (html5-parser::text-node acc)
+		     (t (progn
+			  (when (match-selector matcher element)
+			    (setf acc (append acc (list element))))
+			  acc))))
+		 tree)))
--- test.lisp
+++ test.lisp
@@ -17,24 +17,28 @@
 (5am:def-test match-with-a-simple-class-name ()
   (let ((tree (html5-parser:parse-html5 "<div class=\"element\"></div>")))
     (5am:is (equal (car (cl-html5-query-selector:query-selector ".element" tree))
-                   (html5-parser:node-last-child
-                    (html5-parser:node-last-child
-                     (html5-parser:node-last-child tree)))))))
+		   (html5-parser:node-last-child
+		    (html5-parser:node-last-child
+		     (html5-parser:node-last-child tree)))))))
 
 (5am:def-test match-with-a-simple-id-name ()
   (let ((tree (html5-parser:parse-html5 "<div id=\"element\"></div>")))
     (5am:is (equal (html5-parser:node-last-child
-                    (html5-parser:node-last-child
-                     (html5-parser:node-last-child tree)))
-                   (car (cl-html5-query-selector:query-selector "#element" tree))))))
+		    (html5-parser:node-last-child
+		     (html5-parser:node-last-child tree)))
+		   (car (cl-html5-query-selector:query-selector "#element" tree))))))
 
 (5am:def-test match-with-composed-class-name ()
   (let ((tree (html5-parser:parse-html5 "<div class=\"c1 c2\"></div>")))
     (5am:is (equal (html5-parser:node-last-child
-                    (html5-parser:node-last-child
-                     (html5-parser:node-last-child tree)))
-                   (car (cl-html5-query-selector:query-selector ".c1.c2" tree))))))
+		    (html5-parser:node-last-child
+		     (html5-parser:node-last-child tree)))
+		   (car (cl-html5-query-selector:query-selector ".c1.c2" tree))))))
 
 (5am:def-test must-find-2-element-with-a-class-name ()
   (let ((tree (html5-parser:parse-html5 "<div><div class=\"element\"></div><div class=\"element\"></div></div>")))
     (5am:is (= 2 (length (cl-html5-query-selector:query-selector ".element" tree))))))
+
+(5am:def-test must-find-element-with-tag-name ()
+  (let ((tree (html5-parser:parse-html5 "<div><div class=\"element\"><strong>Ok</strong></div><div class=\"element\"><strong>Ok</strong></div></div>")))
+    (5am:is (= 2 (length (cl-html5-query-selector:query-selector "strong" tree))))))
